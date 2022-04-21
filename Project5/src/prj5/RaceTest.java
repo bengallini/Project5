bc21fa07cb68d9200d6be43d2aa8593ed072fe1b
prakashjawahar <prakashjawahar@localhost>
2022-04-21 17:00:05 -0400

    See this

--- Project5/src/prj5/RaceTest.java
+++ Project5/src/prj5/RaceTest.java
@@ -3,4 +3,14 @@ import student.TestCase;
 
 public class RaceTest extends TestCase{
 
+    private Race race;
+    
+    public void setUp() {
+        race = new Race("white", 100, 50);
+    }
+    
+    public void testGetters() {
+        //comments
+        
+    }
 }
