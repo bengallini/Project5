f9db49db8eb2ff1328b08a7893f7171f30f2789d
bengallini <bengallini@vt.edu>
2022-04-21 22:56:20 -0400

    Bug Testing

--- Project5/src/prj5/FileReader.java
+++ Project5/src/prj5/FileReader.java
@@ -18,28 +18,30 @@ public class FileReader {
 
     private State[] CFRReader(String fileName) throws FileNotFoundException {
         states = new State[NUM_STATES];
-
         File news = new File(fileName);
         Scanner file = new Scanner(news);
         int lineCount = 0;
 
         while (file.hasNextLine() && lineCount < 6) {
-            if (lineCount > 0) {
-                String line = file.nextLine();
+            String line = file.nextLine();
+             if (lineCount > 0) {               
+                System.out.println("Line: " + lineCount);
                 Scanner currLine = new Scanner(line).useDelimiter(",\\s*");
                 String vals[] = new String[11];
 
                 int tokenCount = 0;
                 while (currLine.hasNext() && tokenCount < 11) {
                     vals[tokenCount] = currLine.next();
+                    System.out.println(vals[tokenCount]);
                     tokenCount++;
                 }
                 currLine.close();
                 Integer[] nums = new Integer[10];
                 String name = vals[0];
-                for (int i = 1; i < vals.length; i++) {
+                for (int i = 1; i < vals.length + 1; i++) {
                     nums[i] = NAFinder(vals[i]);
                 }
+                System.out.println(name);
                 int white = nums[1];
                 int black = nums[2];
                 int latinx = nums[3];
@@ -51,14 +53,11 @@ public class FileReader {
                 int asiandeath = nums[9];
                 int otherdeath = nums[10];
                 
-
-
                 Race whiterace = new Race("white", white, whitedeath);
                 Race blackrace = new Race("black", black, blackdeath);
                 Race latinxrace = new Race("latinx", latinx, latinxdeath);
                 Race asianrace = new Race("asian", asian, asiandeath);
                 Race otherrace = new Race("other", other, otherdeath);
-                
                 State tempz = new State(name);
                 tempz.add(whiterace);
                 tempz.add(blackrace);
@@ -67,13 +66,16 @@ public class FileReader {
                 tempz.add(otherrace);
                 
                 states[lineCount] = tempz; 
-                System.out.println(tempz);
+                //System.out.println(tempz);
             } 
+            lineCount++;
         }
+        file.close();
         return states;
     }
     
     private int NAFinder(String str) {
+        
         if (str.equals("NA")) {
             return -1;
         }
--- Project5/src/prj5/Input.java
+++ Project5/src/prj5/Input.java
@@ -5,11 +5,13 @@ import java.io.FileNotFoundException;
 public class Input {
     
     public static void main(String[] args) throws FileNotFoundException {
+        
         if(args.length > 0) {
         FileReader filereader = new FileReader(args[0]);
         }
         else {
             FileReader filereader = new FileReader("Cases_and_Deaths_by_race_CRDT_Sep2020.csv");
         }
+        System.out.println("test");
     }
 }
